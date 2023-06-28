12f5cfc14e1374c9671a316327024ac3e828c9dc
Joshua <joshuamayowa23@yahoo.com>
2023-06-28 10:57:44 +0100

    update

---
+++ facebook/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:facebook/config/palette.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Facebook UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Palette.scaffold,
+      ),
+      // home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
